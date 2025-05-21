bd376991ce612f73e2eaacfa232e83d16d29bf13
Ananya Balakrishnan <ananyabalakrishnan@ucsb.edu>
2025-05-21 15:51:46 -0700

    update continued python code script

--- continued_python_code.py
+++ continued_python_code.py
@@ -1,5 +1,6 @@
 #list commands
-'''
+print("list commands:")
+
 list = [1, 2, 3, 4]
 x = list.append(5) #returns none when trying to print
 
@@ -16,9 +17,31 @@ print(list.copy())
 
 list.sort() #sort the elements of the list in ascending order by default
 print(list.copy())
-'''
+print()
 
 #tuple commands
+print ("tuple commands:")
+
 my_tuple = ("apple", "banana", "cherry", "apple")
+
 print(my_tuple.count("apple")) #counts the amount of times element is in tuple
+
 print(my_tuple.index("cherry")) #finds the index of the first occurence of an element
+print()
+
+#set commands: sets store multiple elements in a single object but do not allow duplicate elements, sets are unordered and unindexed
+#   if you print a set, all elements are printed in a random order
+print ("set commands:")
+vegetables = {"tomato", "potato", "carrot"}
+
+vegetables.add("brocolli") #add a new element in the set
+print(vegetables)
+
+vegetables.discard("tomato") #remove the specified element from the set
+print(vegetables)
+
+vegetables.remove("potato") #removes specified element from the set but diff from discard where will return error if specified element is not there
+print(vegetables)
+
+vegetables.clear() #removes all elements in the set
+print(vegetables)
